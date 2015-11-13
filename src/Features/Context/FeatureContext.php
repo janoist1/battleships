50cfc98d059db2b6fd264fea174a9cebbc44c7a2
Istvan Jano <janoist1@gmail.com>
2015-11-13 13:20:02 +0000

    create basic behat tests

---
+++ src/Features/Context/FeatureContext.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace Worldstores\Battleships\Features\Context;
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\Mink\Driver\BrowserKitDriver;
+use Behat\Mink\Driver\Selenium2Driver;
+use Behat\Mink\Mink;
+use Behat\Mink\Session;
+use Behat\MinkExtension\Context\MinkContext;
+use Symfony\Component\HttpKernel\Client;
+use Worldstores\Battleships\Battleships;
+use Worldstores\Battleships\Model\Point;
+use Worldstores\Battleships\Model\Ship;
+use Worldstores\Battleships\Service\GameService;
+use Symfony\Component\HttpFoundation\Session\Session as HttpSession;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext
+{
+    /** @var Battleships */
+    private $app;
+
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+        $this->app = new Battleships();
+        $this->app['session.test'] = true;
+
+        $this->setMink(new Mink([
+            'silex' => new Session(new BrowserKitDriver(new Client($this->app))),
+//            'selenium' => new Session(new Selenium2Driver('firefox', null, 'http://10.0.2.2:4444/wd/hub')),
+        ]));
+
+        $this->getMink()->setDefaultSessionName('silex');
+    }
+
+    /**
+     * Set size of the board
+     *
+     * @Given there is a :width x :height sized board
+     *
+     * @param $width
+     * @param $height
+     */
+    public function thereIsABoard($width, $height)
+    {
+        $this->getGame()->getBoard()->setDimension(new Point($width, $height));
+        $this->saveGameData();
+    }
+
+    /**
+     * Add a ship onto the board with a given position
+     *
+     * @Given there is a :size length :orientation ship with its first coordinate :coordinate
+     *
+     * @param $size
+     * @param $orientation
+     * @param $coordinate
+     * @throws \Exception
+     */
+    public function thereIsALengthShipWithItsFirstCoordinate($size, $orientation, $coordinate)
+    {
+        $pointA = $this->coordinateToPoint($coordinate);
+
+        switch ($orientation) {
+            case 'horizontal';
+                $pointB = new Point($pointA->getX() + $size - 1, $pointA->getY());
+                break;
+            case 'vertical';
+                $pointB = new Point($pointA->getX(), $pointA->getY() + $size - 1);
+                break;
+            default:
+                throw new \Exception('unknown ship orientation');
+        }
+
+        $this->getGame()->getBoard()->addShip(new Ship($pointA, $pointB, $size));
+        $this->saveGameData();
+    }
+
+    /**
+     * Shoot to a coordinate
+     *
+     * @Then I shoot to :coordinate
+     *
+     * @param $coordinate
+     */
+    public function thenIShootTo($coordinate)
+    {
+        $this->fillField("coordinate", $coordinate);
+        $this->pressButton("submit");
+    }
+
+    /**
+     * Shoot to a the given coordinates
+     *
+     * @Given /^the following shots$/
+     *
+     * @param TableNode $shots
+     */
+    public function givenTheFollowingShots(TableNode $shots)
+    {
+        foreach ($shots->getHash() as $shot) {
+            $this->thenIShootTo($shot['coordinate']);
+        }
+    }
+
+    /**
+     * Dump content
+     *
+     * @Then I dump
+     */
+    public function thenDump()
+    {
+//        echo $this->getMink()->getSession('silex')->getDriver()->getContent() ."\n";
+    }
+
+    /**
+     * Store the game data in the game's session
+     */
+    private function saveGameData()
+    {
+        $gameData = $this->getGame()->getGameData();
+        $this->getHttpSession()->set('gameData', $gameData);
+    }
+
+    /**
+     * Turn coordinate "A5" to Point
+     *
+     * @param $coordinate
+     * @return Point
+     */
+    private function coordinateToPoint($coordinate)
+    {
+        $x = substr($coordinate, 1) * 1;
+        $y = ord(strtoupper(substr($coordinate, 0, 1))) - ord('A') + 1;
+
+        return new Point($x, $y);
+    }
+
+    /**
+     * @return GameService
+     */
+    private function getGame()
+    {
+        return $this->app['game'];
+    }
+
+    /**
+     * @return HttpSession
+     */
+    private function getHttpSession()
+    {
+        return $this->app['session'];
+    }
+}
---
+++ src/Features/game.feature
@@ -0,0 +1,61 @@
+Feature: Play the game
+  The application should accept input from the user in the format "A5" to signify a square to target,
+  and feedback to the user whether the shot was success, miss, and additionally report on the sinking of any vessels.
+
+  Background:
+    Given there is a 10 x 10 sized board
+    And there is a 3 length vertical ship with its first coordinate A5
+    And there is a 4 length horizontal ship with its first coordinate F4
+    And I am on homepage
+    Then I should see "Enter coordinates"
+
+  Scenario: Hit
+    When I shoot to A5
+    Then I should see "hit a5"
+
+  Scenario: Miss
+    When I shoot to A6
+    Then I should see "miss"
+
+  Scenario: Sunk
+    Given the following shots
+      | coordinate |
+      | A5         |
+      | B5         |
+      | C5         |
+    Then I should see "sunk"
+
+  Scenario: Win with no miss
+    Given the following shots
+      | coordinate |
+      | A5         |
+      | B5         |
+      | C5         |
+      | F4         |
+      | F5         |
+      | F6         |
+      | F7         |
+    Then I should see "Well done! You completed the game in 7 shots"
+
+  Scenario: Win with a miss
+    Given the following shots
+      | coordinate |
+      | A1         |
+      | A5         |
+      | B5         |
+      | C5         |
+      | F4         |
+      | F5         |
+      | F6         |
+      | F7         |
+    Then I should see "Well done! You completed the game in 8 shots"
+
+  Scenario: Entering show
+    When I fill in "coordinate" with "show"
+    And I press "submit"
+    Then I should see "showing ships"
+
+  Scenario: Entering reset
+    When I fill in "coordinate" with "reset"
+    And I press "submit"
+    Then I should see "game restarted"
